2decdb8017bea8f1f03275bfdeb59bb70c1b6f21
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-15 15:14:32 +0300

    task-02 done

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,33 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsListRef = document.querySelector("ul#ingredients");
+
+const makeIngredientMarkup = (ingredient) => {
+  const ingredientElement = document.createElement("li");
+  ingredientElement.classList.add("item");
+  ingredientElement.textContent = ingredient;
+  return ingredientElement;
+};
+
+const ingredientsMarkup = ingredients.map(makeIngredientMarkup);
+
+ingredientsListRef.append(...ingredientsMarkup);
+
+// Заборонений спосіб з парсом строки))
+
+// const ingredientsListRef = document.querySelector("ul#ingredients");
+
+// const ingredientsMarkup = ingredients
+//   .map((ingredient) => {
+//     return `<li class = "item">${ingredient}</li>`;
+//   })
+//   .join("");
+
+// ingredientsListRef.innerHTML = ingredientsMarkup;
