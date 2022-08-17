4eae91f48176499c8746c8653fc61ff6ca6b578b
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-17 17:10:15 +0300

    task-08 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginFormRef = document.querySelector(".login-form");
 
+const saveInputData = (email, password) => {
+  const inputData = {};
+  inputData.email = email;
+  inputData.password = password;
+  console.log(inputData);
+};
+
+const onLoginFormSubmit = (event) => {
+  event.preventDefault();
+  const formEmail = event.currentTarget.elements.email.value;
+  const formPassword = event.currentTarget.elements.password.value;
+  const isFormFilledCorrect = !(formEmail === "") && !(formPassword === "");
+
+  isFormFilledCorrect
+    ? saveInputData(formEmail, formPassword)
+    : alert("Всі поля повинні бути заповнені не халтурь))");
+};
+
+loginFormRef.addEventListener("submit", onLoginFormSubmit);
