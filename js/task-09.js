7da5a57ec9e6b40e04572b53f33ace65f420be45
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-17 17:32:41 +0300

    task-09 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
+const refs = {
+  changeColorBtn: document.querySelector(".change-color"),
+  currentColorWidget: document.querySelector(".color"),
+  body: document.querySelector("body"),
+};
+
+const onChangeColorBtn = () => {
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.currentColorWidget.textContent = color;
+};
+
+refs.changeColorBtn.addEventListener("click", onChangeColorBtn);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
