126d447227f42fe18cd3f5b8791f8207fea1731c
vitalii-khymchuk <vitalii.workbox001@gmail.com>
2022-08-18 19:16:31 +0300

    debug_v1.00

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,14 @@
 const inputRef = document.querySelector("#validation-input");
 
+const changeClass = (newClass, oldClass) => {
+  inputRef.classList.add(newClass);
+  inputRef.classList.remove(oldClass);
+};
+
 const onInputValidation = (event) => {
-  if (
-    Number(inputRef.dataset.length) === Number(event.currentTarget.value.length)
-  ) {
-    inputRef.classList.add("valid");
-    inputRef.classList.remove("invalid");
-  } else {
-    inputRef.classList.add("invalid");
-    inputRef.classList.remove("valid");
-  }
+  Number(inputRef.dataset.length) === Number(event.currentTarget.value.length)
+    ? changeClass("valid", "invalid")
+    : changeClass("invalid", "valid");
 };
 
 inputRef.addEventListener("blur", onInputValidation);
